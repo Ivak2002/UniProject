91113da7eef1e406c0db97a320d446235e0943fa
Ivak2002 <Ivaka2002@students.softuni.bg>
2025-07-25 23:52:50 +0300

    Logging out is working now + dropdown menu for future plans is done too

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from .forms import RegisterForm,CustomLoginForm
@@ -24,4 +25,9 @@ class UserLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('home')
 
+class CustomLogoutView(LogoutView):
+    http_method_names = ['post']
+    next_page = 'home'
 
+class SettingsView(TemplateView):
+    template_name = 'settings.html'
