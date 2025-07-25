5c344011687c26c8fbabf6c09ec6ec9401d2b3d4
Ivak2002 <Ivaka2002@students.softuni.bg>
2025-07-25 22:32:00 +0300

    Fully working login / register !

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django import forms
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    birth_date = models.DateField(null = True, blank = False)
