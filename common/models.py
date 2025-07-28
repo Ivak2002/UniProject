0a7212962be32526b1a18e963f41377cb48fafc3
Ivak2002 <Ivaka2002@students.softuni.bg>
2025-07-28 15:47:06 +0300

    Help Form works properly now

--- common/models.py
+++ common/models.py
@@ -1,3 +1,35 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
+from account.models import CustomUser
 
-# Create your models here.
+class HelpModel(models.Model):
+    question_theme = models.TextField(
+        max_length=30,
+        blank=False,
+        null=False,
+        validators=[
+            MinLengthValidator(5),
+        ],
+    )
+    email = models.EmailField(
+        blank=False,
+        null=False,
+    )
+    question = models.TextField(
+        blank=False,
+        null=False,
+        validators=[
+            MinLengthValidator(5),
+            MaxLengthValidator(200),
+        ]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    current_sender = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='help_requests'
+    )
+
+    def __str__(self):
+        return f"{self.question_theme} from {self.current_sender}"
