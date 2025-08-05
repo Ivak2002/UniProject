eb391969d5148b32b47148a6b7c7fa567e2414ab
Ivak2002 <Ivaka2002@students.softuni.bg>
2025-08-05 18:36:00 +0300

    More Changes

--- staticfiles/menu.js
+++ staticfiles/menu.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const userMenu = document.getElementById('userMenu');
-  const userMenuContent = document.getElementById('userMenuContent');
+    const userMenu = document.getElementById('userMenu');
+    const userMenuContent = document.getElementById('userMenuContent');
+    const topNavBurger = document.getElementById('topNavBurger');
+    const topNavMenu = document.getElementById('topNavMenu');
+    const productNavBurger = document.getElementById('productNavBurger');
+    const productNavMenu = document.getElementById('productNavMenu');
 
-  if (!userMenu || !userMenuContent) return;
+    if (userMenu && userMenuContent) {
+        userMenu.querySelector('i').addEventListener('click', (e) => {
+            e.stopPropagation();
+            userMenuContent.classList.toggle('show');
+        });
 
+        document.addEventListener('click', () => {
+            userMenuContent.classList.remove('show');
+        });
 
-  userMenu.querySelector('i').addEventListener('click', (e) => {
-    e.stopPropagation();
-    userMenuContent.classList.toggle('show');
-  });
+        userMenuContent.addEventListener('click', (e) => {
+            e.stopPropagation();
+        });
+    }
 
-
-  document.addEventListener('click', () => {
-    userMenuContent.classList.remove('show');
-  });
-
-
-  userMenuContent.addEventListener('click', (e) => {
-    e.stopPropagation();
-  });
+    if (topNavBurger && topNavMenu) {
+        topNavBurger.addEventListener('click', () => {
+            topNavMenu.classList.toggle('active');
+        });
+    }
+    if (productNavBurger && productNavMenu) {
+        productNavBurger.addEventListener('click', () => {
+            productNavMenu.classList.toggle('active');
+        });
+    }
 });
-
